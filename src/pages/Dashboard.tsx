615386b99bc78e779e04b9b58697e4203892c903
AhmadTawbi <ahmad.tawbi.93@hotmail.com>
2022-04-25 04:31:03 +0300

    design

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -14,7 +14,7 @@ export const Dashboard = () => {
                 <h2>Dashboard</h2>
             </div>
             <p className='defaultContent'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                Dashboards content to be added soon....
             </p>
         </Content>} />
     )
